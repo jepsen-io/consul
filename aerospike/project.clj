1fbf37a549237762d08baaade3a69f5b96ecea4a
Aphyr <aphyr@aphyr.com>
2015-07-28 12:21:03 -0700

    refactor consul into own project

--- aerospike/project.clj
+++
@@ -1,8 +0,0 @@
-(defproject aerospike "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [jepsen "0.0.4-SNAPSHOT"]
-                 [com.aerospike/aerospike-client "3.1.0"]])
